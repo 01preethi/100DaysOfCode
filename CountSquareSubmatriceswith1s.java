11c97e4b3050a2d2a3c2d6d4bd71008dc50ba4fd
Preethi muthu <146505666+01preethi@users.noreply.github.com>
2025-08-20 21:24:25 +0530

    Add files via upload

---
+++ CountSquareSubmatriceswith1s.java
@@ -0,0 +1,31 @@
+package coreJava;
+public class CountSquareSubmatriceswith1s {
+ public static void main(String[] args) {
+	 int[][]matrix =
+		 {
+			   {0,1,1,1},
+			   {1,1,1,1},
+			   {0,1,1,1}};	
+	 System.out.print(countSquares(matrix));
+ }
+ public static int countSquares(int[][] matrix) {
+     int m = matrix.length;
+     int n = matrix[0].length;
+     int[][] dp = new int[m][n];
+     int count = 0;
+     for(int i=0; i<m; i++){
+         for(int j=0; j<n; j++){
+             if(matrix[i][j]==1){
+                 if(i==0 || j==0)
+                     dp[i][j] = 1;
+                 else
+                     dp[i][j] = Math.min(Math.min(dp[i-1][j],
+                     dp[i][j-1]),dp[i-1][j-1])+1;
+             }
+             count+=dp[i][j];
+         }
+     }
+     return count;
+ }
+
+}
