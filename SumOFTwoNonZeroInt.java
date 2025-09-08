dce87344bb13e93461e62db2566b48c416b0783a
Preethi muthu <146505666+01preethi@users.noreply.github.com>
2025-09-08 13:09:15 +0530

    SumOFTwoNonZeroInt.java

--- SumOFTwoNonZeroInt.java
+++ SumOFTwoNonZeroInt.java
@@ -18,26 +18,27 @@ public class SumOFTwoNonZeroInt {
 }
 
 method - 2
-//public class SumOFTwoNonZeroInt {
-//	 public static void main(String[] args) {
-//		int n = 5;
-//		int[] res = getNoZeroIntegers(n);
-//		System.out.println(Arrays.toString(res));
-//	 }
-// public static int[] getNoZeroIntegers2(int n) {
-//	 for(int a=1; a<n; a++){
-//         int b = n-a;
-//         if(hasZero(a) && hasZero(b)){
-//             return new int[] {a,b};
-//         }
-//     }
-//     return new int[] {};
-// }
-// private static boolean hasZero(int x){
-//     while(x>0){
-//         if(x%10 ==0) return false;
-//         x/=10;
-//     }
-//     return true;
-// }
-//}
+public class SumOFTwoNonZeroInt {
+	 public static void main(String[] args) {
+		int n = 5;
+		int[] res = getNoZeroIntegers(n);
+		System.out.println(Arrays.toString(res));
+	 }
+public static int[] getNoZeroIntegers2(int n) {
+	 for(int a=1; a<n; a++){
+        int b = n-a;
+        if(hasZero(a) && hasZero(b)){
+            return new int[] {a,b};
+        }
+    }
+    return new int[] {};
+}
+private static boolean hasZero(int x){
+    while(x>0){
+        if(x%10 ==0) return false;
+        x/=10;
+    }
+    return true;
+}
+}
+
